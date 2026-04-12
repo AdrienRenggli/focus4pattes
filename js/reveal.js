2dd45a9af083681cc41e3edb0b826d9f1927fce3
Adrien Renggli <adrien.renggli@gmail.com>
2026-04-12 14:30:03 +0200

    feat(reveal): get tel & mail for display

---
+++ js/reveal.js
@@ -0,0 +1,12 @@
+// Partially hide the phone number and email address to bots
+// They are never fully shown on the code.
+
+var tel = ["41", "79", "512", "12", "96"];
+document.getElementById("phone").innerHTML = "+" + tel.join(" ");
+document.getElementById("phoneLink").setAttribute("href", "tel:+" + tel.join(""));
+
+var user = ["love", "photos774"];
+var domain = ["gmail", "com"];
+var email = user.join(".") + "@" + domain.join(".");
+document.getElementById("email").innerHTML = email;
+document.getElementById("emailLink").setAttribute("href", "mailto:" + email);
